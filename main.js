9bd24fa0050f26c26234c3772b5b37c295d74d8f
Alyx <alyx63@gmail.com>
2021-10-17 15:09:53 +0400

    1

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const Player1 = {
   img: '',
   weapon: 'Ice Sword',
   attack: function() {
-  console.log(name+'Fight...');
-  },
+  console.log(Player1.name + ' Fight...');
+  }
 };
+const Player2 = {
+  name: 'Scorpion',
+  hp: 100,
+  img: '',
+  weapon: 'Fireball',
+  attack: function() {
+  console.log(Player2.name + ' Fight...');
+  }
+};
+Player1.attack();
+Player2.attack();
